592ddb89bf651e6153836931bd516aaf78180d89
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-04 09:37:49 -0300

    add buffers keymaps

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -5,4 +5,9 @@ vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 
+vim.keymap.set('n', '<S-l>', ':BufferNext<CR>')
+vim.keymap.set('n', '<S-h>', ':BufferPrevious<CR>')
+vim.keymap.set('n', '<leader>bc', ':BufferClose<CR>')
+vim.keymap.set('n', '<leader>bC', ':BufferCloseAllButCurrent<CR>')
+
 

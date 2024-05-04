d5c6bec4507128c2fecccf5a5b0f4b77fbf2c99f
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-04 07:58:32 -0300

    init configs

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			-- lspconfig.tsserver.setup({
+			--  capabilities = capabilities
+			-- })
+
+			require("typescript-tools").setup({
+				settings = {
+					tsserver_plugins = {
+						"@styled/typescript-styled-plugin",
+					},
+				},
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}

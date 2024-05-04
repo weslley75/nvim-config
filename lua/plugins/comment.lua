1aa1151263b1c266944d920b8777b8bb85ead33a
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-04 08:55:42 -0300

    add comment toggler

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,20 @@
+return {
+	"numToStr/Comment.nvim",
+	lazy = false,
+	config = function()
+		require("Comment").setup({
+			toggler = {
+				line = "<leader>/",
+				block = "<leader>*",
+			},
+      opleader = {
+        line = "<leader>/",
+        block = "<leader>*",
+      },
+      mappings = {
+        basic = true,
+        extra = false,
+      }
+		})
+	end,
+}

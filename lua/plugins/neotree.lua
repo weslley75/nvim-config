9c19d99999a69971d23423efc9ff72c1c9663eae
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-04 08:42:19 -0300

    change neotree keymap

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,6 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle<CR>', {})
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', {})
   end
 }

a8f80cf9230319e707d1d8842ff5d18720589066
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-06 20:51:13 -0300

    update packages

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },

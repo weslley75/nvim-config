bd7ecfa174c1e838acd90566296be795bd8c7119
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-06 18:41:39 -0300

    install neodev

---
+++ lua/plugins/neodev.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/neodev.nvim",
+  opts = {},
+  config = function()
+    require("neodev").setup()
+
+    local lspconfig = require("lspconfig")
+
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace",
+          },
+        },
+      },
+    })
+  end,
+}

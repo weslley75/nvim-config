20a22b75dbed73e33bb2111ffbe0198859ab9a57
Weslley Almeida <weslleyalmeida6@gmail.com>
2024-05-06 22:43:09 -0300

    feat: Add CopilotChat.nvim to dependencies
    This commit adds CopilotChat.nvim to the list of dependencies in
    lazy-lock.json and copilot.lua. The branch used is "canary". It also
    includes the setup configuration for CopilotChat in copilot.lua.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,17 @@
 return {
   "github/copilot.vim",
+  dependencies = {
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      config = function()
+        require("CopilotChat").setup()
+      end,
+    },
+  },
   config = function()
     vim.cmd([[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]])
   end,

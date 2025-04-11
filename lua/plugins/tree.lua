feede9373b438125a8a4e6e836ebcf5637ea6340
Weslley <weslleyalmeida6@gmail.com>
2025-04-10 21:03:05 -0300

    feat(nvim-tree): configure git and visibility options
    Configure nvim-tree to show git-ignored files, dotfiles, git status
    icons and highlighting. Adjust renderer settings for better
    visibility of special files and symlinks.

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -4,7 +4,29 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup({})
+    require("nvim-tree").setup({
+      git = {
+        ignore = false, -- Show files that are in .gitignore
+      },
+      renderer = {
+        icons = {
+          git_placement = "before",
+          show = {
+            git = true,
+          },
+        },
+        highlight_git = true,
+        special_files = {},
+        symlink_destination = true,
+        indent_markers = {
+          enable = false,
+        },
+      },
+      filters = {
+        dotfiles = false, -- Show dotfiles
+        git_ignored = false,
+      },
+    })
 
     local api = require("nvim-tree.api")
 
